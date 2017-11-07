4f8e60850b3860086052a770f119bc513613d215
ruineringing <ruinering@bk.ru>
2017-11-07 03:26:24 +0300

    Add CommandLine

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -14,12 +14,16 @@ namespace WebApplication1
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+
+            host.Run();
+        }
     }
 }
